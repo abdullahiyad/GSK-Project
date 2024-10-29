6568090af99cb979cd515e21203c86b903a51274
Faisal <zkryfysl@gmail.com>
2024-10-28 13:18:39 -1200

    adding loading to fav , home

---
+++ js/loadingPage.js
@@ -0,0 +1,8 @@
+// loading page
+window.onload = function() {
+    // Hide the loading container
+    document.querySelector('.loading-container').style.display = 'none';
+    // Show the main content
+    document.querySelector('header').style.display = 'flex';
+    document.querySelector('main').style.display = 'block';
+  }
